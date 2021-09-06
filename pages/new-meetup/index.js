4d78f066989ad54431ff28c342eb53057d16eddb
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-09-06 17:13:01 +0100

    define reuasable component

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+
+export default function NewMeetup() {
+    return (
+        <NewMeetupForm />
+    )
+}
