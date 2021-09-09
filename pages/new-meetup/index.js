a78c5a1bbbef0b2f0a15c761e33d7b7e31bdc107
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-09-09 12:32:07 +0100

    use POST meetup api to insert new one

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,22 @@
+import { useRouter } from 'next/dist/client/router';
 import React from 'react'
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
 
 export default function NewMeetup() {
+    const router=useRouter()
+    async function addMeetup(newMeetupData) {
+        console.log("newMeetupData", newMeetupData);
+        const response = await fetch('/api/new-meetup', {
+            body: JSON.stringify(newMeetupData),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(" response json data", data);
+        router.push('/')
 
-    function addMeetup(data){
-        console.log("data", data);
     }
     return (
         <NewMeetupForm onAddMeetup={addMeetup} />
