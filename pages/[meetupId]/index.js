7816f2d94e71589fb7e304a545c3e1f9359d3b9d
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-09-09 14:11:37 +0100

    getStaticPaths ids dynamicly from mongodb

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 
 import MeetupDetailsComponent from "../../components/meetups/MeetupDetail"
-
+import {MongoClient} from "mongodb"
 
 export default function MeetupDetails({ meetupData }) {
     return (
@@ -9,29 +9,43 @@ export default function MeetupDetails({ meetupData }) {
 }
 
 export async function getStaticPaths() {
+    const client = await MongoClient.connect("mongodb+srv://mehdi:0000@cluster0.hqkwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    const db = client.db()
+    const meetupsCollection = db.collection('meetups')
+    // find first argument for filter createra sooo => {} will fetch all element
+    // find second argument for field  
+    const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray()
+
+    client.close()
+
     return {
         fallback: false,
         //=> paths contains all the id and if user type inexistinting id => redirect to 404
         // fallback: true,
         // => next will try generate page for this inexistinting id from server while request
 
-        paths: [
-            {
-                params: {
-                    meetupId: 'm1'
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm2'
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm3'
-                }
+        paths: meetups.map(item => ({
+            params: {
+                meetupId: item._id.toString()
             }
-        ]
+        }))
+        // paths: [
+        //     {
+        //         params: {
+        //             meetupId: 'm1'
+        //         }
+        //     },
+        //     {
+        //         params: {
+        //             meetupId: 'm2'
+        //         }
+        //     },
+        //     {
+        //         params: {
+        //             meetupId: 'm3'
+        //         }
+        //     }
+        // ]
     }
 
 }
