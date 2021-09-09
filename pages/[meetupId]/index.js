98de0bbbc8a0f610919be81b5706e4fccfe33042
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-09-09 09:23:40 +0100

    static meetup item page + navigation to meetup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+
+import MeetupDetailsComponent from "../../components/meetups/MeetupDetail"
+
+
+export default function MeetupDetails() {
+    return (
+        <MeetupDetailsComponent image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" a
+        title="first meetup yeaaaaaa"
+        address="chotrana 1 ariana"
+        description="Bloublba blablalb description"
+        />
+    )
+}
