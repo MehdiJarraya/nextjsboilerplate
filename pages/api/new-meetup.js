cbe5a72ab8102a28d6b8130a96af2c6cea997a27
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-09-09 12:30:16 +0100

    connect to mongodb client and db + create POST meetup api

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+//URL is =>  /api/new-meetup
+
+import {MongoClient} from 'mongodb'
+
+
+async function handler(req, res){
+    // POST /api/new-meetup
+    if(req.method==='POST'){
+        const data=req.body;
+        // const {title, image, address, description}=data
+        const client= await  MongoClient.connect("mongodb+srv://mehdi:0000@cluster0.hqkwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        const db=client.db();
+        const meetupsCollection = db.collection('meetups')
+       const result = await meetupsCollection.insertOne(data);
+       console.log("result", result);
+       client.close()
+       res.status(201).json({message:"Meetup inserted sucesseffly"})
+    }
+}
+
+export default handler
